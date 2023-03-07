0b335260e5d3376629fd364fa0df5d6068ad22f9
William Gadd <wgadd@sasktel.net>
2023-03-07 12:25:25 -0600

    Import utils css module and  add tags and classes to make the blog articles look good

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -3,6 +3,8 @@ import Head from 'next/head';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 import Date from '../../components/date';
 
+import utilStyles from '../../styles/utils.module.css';
+
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id);
   return {
@@ -21,15 +23,18 @@ export async function getStaticPaths() {
 }
 
 export default function Post({ postData }) {
-  return <Layout>
-    <Head>
-      <title>{postData.title}</title>
-    </Head>
-    {postData.title}
-    <br />
-    {postData.id}
-    <br />
-    <Date dateString={postData.date} />
-    <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-  </Layout>;
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
 }
