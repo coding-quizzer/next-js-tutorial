8ab765170cd0cb38eed8157d4a22761a9f89c185
William Gadd <wgadd@sasktel.net>
2023-03-07 12:20:03 -0600

    Add new Date component to blog article pages

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,6 +1,7 @@
 import Layout from '../../components/layout';
 import Head from 'next/head';
 import { getAllPostIds, getPostData } from '../../lib/posts';
+import Date from '../../components/date';
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id);
@@ -28,7 +29,7 @@ export default function Post({ postData }) {
     <br />
     {postData.id}
     <br />
-    {postData.date}
+    <Date dateString={postData.date} />
     <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
   </Layout>;
 }
